57c98b0a9bfe750d4d7b028346a0fd1f73ba5407
Eliot <100833504+eliotdgl@users.noreply.github.com>
2025-03-27 01:12:02 +0100

    Update stock-tickers.py

--- data/stock-tickers.py
+++ data/stock-tickers.py
@@ -46,22 +46,37 @@ stock_tickers = {symbol for symbol in stock_tickers if not (symbol[-1] == '$')}
 stock_tickers = {symbol for symbol in stock_tickers if len(symbol) >= 2} # So it does not consider start of sentence as tickers
 
 
-stock_indices = ["S&P 500", "S&P500", "Standard & Poor's 500",
-                 "Dow Jones Industrial Average", "DJIA", "Dow Jones", "The Dow",
-                 "Nasdaq", "NASDAQ",
-                 "FTSE 100", "FTSE100",
-                 "Nikkei 225", "Nikkei",
-                 "DAX",
-                 "CAC 40",
-                 "Hang Seng", "HSI",
-                 "ASX 200",
-                 "Bovespa", "Ibovespa",
-                 "Sensex", "BSE Sensex",
-                 "TSX",
-                 "KOSPI",
-                 "Russell 3000",
-                 "MSCI"
-]
+tokenized_stock_indices = {
+    "S&P 500": "<FinGPTICKER_S&P500>",
+    "S&P500": "<FinGPTICKER_S&P500>",
+    "Standard & Poor's 500": "<FinGPTICKER_S&P500>",
+
+    "Dow Jones Industrial Average": "<FinGPTICKER_DowJones>",
+    "DJIA": "<FinGPTICKER_DowJones>",
+    "Dow Jones": "<FinGPTICKER_DowJones>",
+    "The Dow": "<FinGPTICKER_DowJones>",
+
+    "Nasdaq": "<FinGPTICKER_NASDAQ>",
+    "NASDAQ": "<FinGPTICKER_NASDAQ>",
+
+    "FTSE 100": "<FinGPTICKER_FTSE100>",
+    "FTSE100": "<FinGPTICKER_FTSE100>",
+
+    "Nikkei 225": "<FinGPTICKER_Nikkei>",
+    "Nikkei": "<FinGPTICKER_Nikkei>",
+
+    "DAX": "<FinGPTICKER_DAX>",
+
+    "CAC 40": "<FinGPTICKER_CAC40>",
+
+    "Hang Seng": "<FinGPTICKER_HSI>",
+    "HSI": "<FinGPTICKER_HSI>",
+
+    "Russell 3000": "<FinGPTICKER_Russell>",
+    "Russell": "<FinGPTICKER_Russell>",
+
+    "MSCI": "<FinGPTICKER_MSCI>"
+}
 
 
 # Create dictionary {ticker: company_name}
