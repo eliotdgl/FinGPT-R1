f367746e668e73d1248353770cbd0f738a6c4199
Eliot <100833504+eliotdgl@users.noreply.github.com>
2025-04-02 16:26:08 +0200

    Update get_stock_tickers.py

--- tokenization/stock_tickers/get_stock_tickers.py
+++ tokenization/stock_tickers/get_stock_tickers.py
@@ -46,39 +46,7 @@ stock_tickers = {symbol for symbol in stock_tickers if not (symbol[-1] == '$')}
 stock_tickers = {symbol for symbol in stock_tickers if len(symbol) >= 2} # So it does not consider start of sentence as tickers
 
 
-tokenized_stock_indices = {
-    "S&P 500": "<FinGPTICKER_S&P500>",
-    "S&P500": "<FinGPTICKER_S&P500>",
-    "Standard & Poor's 500": "<FinGPTICKER_S&P500>",
-
-    "Dow Jones Industrial Average": "<FinGPTICKER_DowJones>",
-    "DJIA": "<FinGPTICKER_DowJones>",
-    "Dow Jones": "<FinGPTICKER_DowJones>",
-    "The Dow": "<FinGPTICKER_DowJones>",
-
-    "Nasdaq": "<FinGPTICKER_NASDAQ>",
-    "NASDAQ": "<FinGPTICKER_NASDAQ>",
-
-    "FTSE 100": "<FinGPTICKER_FTSE100>",
-    "FTSE100": "<FinGPTICKER_FTSE100>",
-
-    "Nikkei 225": "<FinGPTICKER_Nikkei>",
-    "Nikkei": "<FinGPTICKER_Nikkei>",
-
-    "DAX": "<FinGPTICKER_DAX>",
-
-    "CAC 40": "<FinGPTICKER_CAC40>",
-
-    "Hang Seng": "<FinGPTICKER_HSI>",
-    "HSI": "<FinGPTICKER_HSI>",
-
-    "Russell 3000": "<FinGPTICKER_Russell>",
-    "Russell": "<FinGPTICKER_Russell>",
-
-    "MSCI": "<FinGPTICKER_MSCI>"
-}
-
-
+"""
 # Create dictionary {ticker: company_name}
 def get_company_name(ticker):
     try:
@@ -91,3 +59,4 @@ def get_company_name(ticker):
 ticker_name_dict = {}
 for ticker in tqdm(stock_tickers, desc="Fetching Company Names"):
     ticker_name_dict[ticker] = get_company_name(ticker)
+"""
