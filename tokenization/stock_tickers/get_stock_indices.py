c149653386467feb3820be4b947b339aa54a081a
Eliot <100833504+eliotdgl@users.noreply.github.com>
2025-04-02 16:25:43 +0200

    Create get_stock_indices.py

---
+++ tokenization/stock_tickers/get_stock_indices.py
@@ -0,0 +1,31 @@
+tokenized_stock_indices = {
+    "S&P 500": "<FinGPTICKER_S&P500>",
+    "S&P500": "<FinGPTICKER_S&P500>",
+    "Standard & Poor's 500": "<FinGPTICKER_S&P500>",
+
+    "Dow Jones Industrial Average": "<FinGPTICKER_DowJones>",
+    "DJIA": "<FinGPTICKER_DowJones>",
+    "Dow Jones": "<FinGPTICKER_DowJones>",
+    "The Dow": "<FinGPTICKER_DowJones>",
+
+    "Nasdaq": "<FinGPTICKER_NASDAQ>",
+    "NASDAQ": "<FinGPTICKER_NASDAQ>",
+
+    "FTSE 100": "<FinGPTICKER_FTSE100>",
+    "FTSE100": "<FinGPTICKER_FTSE100>",
+
+    "Nikkei 225": "<FinGPTICKER_Nikkei>",
+    "Nikkei": "<FinGPTICKER_Nikkei>",
+
+    "DAX": "<FinGPTICKER_DAX>",
+
+    "CAC 40": "<FinGPTICKER_CAC40>",
+
+    "Hang Seng": "<FinGPTICKER_HSI>",
+    "HSI": "<FinGPTICKER_HSI>",
+
+    "Russell 3000": "<FinGPTICKER_Russell>",
+    "Russell": "<FinGPTICKER_Russell>",
+
+    "MSCI": "<FinGPTICKER_MSCI>"
+}
