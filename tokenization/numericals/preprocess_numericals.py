ec7b5c75530bdc42a015081fc2d48ab18ac794f9
Bilal Bounajma <bbounajma@student.ethz.ch>
2025-04-03 20:17:41 +0200

    Process numbers to put everything together

--- tokenization/numericals/preprocess_numericals.py
+++ tokenization/numericals/preprocess_numericals.py
@@ -9,10 +9,12 @@ def is_number(tokens : str) -> bool:
 def round_number(tokens : str) -> str:
   """
     Round the number keeping only the first half of the digits
+    Can change precision to change the acceptable length
   """
-  precision = len(tokens) // 2
+  precision = len(tokens) // 2 +1
 
   if bool(re.search(r"\.", tokens)):
+    precision-=1
     [integ, decim] = tokens.split('.', 1) if '.' in tokens else [tokens, ""]
     if len(integ) >= precision:
       result = integ[:precision] + ("#" * (len(integ) - precision))
@@ -24,7 +26,24 @@ def round_number(tokens : str) -> str:
     result = tokens[:precision] + ("#" * (len(tokens) - precision))
   return result
 
-
+def merge_numbers(text):
+    tokens = text.split()
+    result = []
+    buffer = []
+    
+    for token in tokens:
+        if token.isdigit():
+            buffer.append(token)
+        else:
+            if buffer:
+                result.append(''.join(buffer))
+                buffer = []
+            result.append(token)
+    
+    if buffer:
+        result.append(''.join(buffer))
+    
+    return ' '.join(result)
 
 def replace_numbers(text):
   """
@@ -145,6 +164,34 @@ def replace_numbers(text):
   return ' '.join(new_words)
   
 
+def proccess_numbers(token :str,splited=False)-> str :
+    """
+    Identify numbers in text,replace them with their numerical values and round them. 
+    Then, if splited, return the preprocessed text, the numerical part and the qualitative part.
+    Otherwise, return the preprocessed text.
+
+    """
+    temp=str(replace_numbers(token))
+    print(temp)
+    temp = merge_numbers(temp)
+    result=''
+    numericals=''
+    qualitatives=''
+    for word in temp.split():
+        if is_number(word):
+            word2 = round_number(word)
+            numericals+=word2+' '
+        else :
+          qualitatives+=word+' '
+          word2=word
+        result+=word2+' '
+    if splited:
+      return [result, numericals, qualitatives]
+    else:
+      return result
+    
+        
+
 print("We can identify numbers:")
 print("Is 1234 a number ?", is_number("123"))
 print("Is 1.234 a number ?", is_number("1.23"))
@@ -159,3 +206,4 @@ print("We also change numbers written in words to their numeric representation:"
 print("test one->",str(replace_numbers("test one")))
 print("test twenty two->",str(replace_numbers("test twenty two")))
 print("test one hundred and half a thousand->",str(replace_numbers("test one hundred and half a thousand")))
+print(proccess_numbers("test 1234 and 1.234 and 1 234 and 1.234567 and 123456.7"))
