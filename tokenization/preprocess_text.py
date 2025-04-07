e2f357255f1160e52c0093c92c306437471761a8
Eliot <100833504+eliotdgl@users.noreply.github.com>
2025-04-07 21:55:04 +0200

    Update preprocess_text.py

--- tokenization/preprocess_text.py
+++ tokenization/preprocess_text.py
@@ -4,6 +4,6 @@ from tokenization.numericals.preprocess_numericals import preproccess_numbers
 
 def preprocess_text(text: str)->str:
     preprocessed_text = preproccess_numbers(text)
-    preprocessed_text = preprocess_stocks(text)
+    preprocessed_text = preprocess_stocks(preprocessed_text)
 
     return preprocessed_text
