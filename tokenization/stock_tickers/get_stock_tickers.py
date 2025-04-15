ba3595e996dd43ebeb9044d8146b8a0f28e8b3c3
eliotdgl <eliot.dugelay@gmail.com>
2025-04-15 16:53:05 +0200

    Updates stock tickers

--- tokenization/stock_tickers/get_stock_tickers.py
+++ tokenization/stock_tickers/get_stock_tickers.py
@@ -1,16 +1,19 @@
-from yahoo_fin import stock_info as si
-import yfinance as yf
 import pandas as pd
-from tqdm import tqdm
 
 
 # Main stock tickers
-df_sp500 = pd.DataFrame(si.tickers_sp500())
-#df_nasdaq = pd.DataFrame(si.tickers_nasdaq())
-#df_dow = pd.DataFrame(si.tickers_dow())
-#df_other = pd.DataFrame(si.tickers_other())
+sp500=pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
+sp500["Symbol"] = sp500["Symbol"]
 
-""" Other tickers sources
+""" # Using yahoo_fin
+from yahoo_fin import stock_info as si
+
+df_sp500 = pd.DataFrame(si.tickers_sp500()) #Doesn't work for S&P500
+df_nasdaq = pd.DataFrame(si.tickers_nasdaq())
+df_dow = pd.DataFrame(si.tickers_dow())
+df_other = pd.DataFrame(si.tickers_other())
+
+ # Other tickers sources
 tickers_ftse100
 tickers_ftse250
 tickers_ibovespa
@@ -19,14 +22,17 @@ tickers_niftybank
 """
 
 # Convert to sets
-set_sp500 = set(symbol for symbol in df_sp500[0].values.tolist())
-#set_nasdaq = set(symbol for symbol in df_nasdaq[0].values.tolist())
-#set_dow = set(symbol for symbol in df_dow[0].values.tolist())
-#set_other = set(symbol for symbol in df_other[0].values.tolist())
+set_sp500 = set(sp500.Symbol.tolist())
 
+""" # Using yahoo_fin
+set_sp500 = set(symbol for symbol in df_sp500[0].values.tolist())
+set_nasdaq = set(symbol for symbol in df_nasdaq[0].values.tolist())
+set_dow = set(symbol for symbol in df_dow[0].values.tolist())
+set_other = set(symbol for symbol in df_other[0].values.tolist())
+"""
 
 # Union of all stock tickers
-raw_stock_tickers = set.union(set_sp500) #set_nasdaq, set_dow, set_other)
+raw_stock_tickers = set.union(set_sp500) # (set_nasdaq, set_dow, set_other)
 raw_stock_tickers.discard('')
 
 # Clean stock tickers
@@ -45,6 +51,7 @@ stock_tickers = {symbol for symbol in stock_tickers if len(symbol) >= 2} # So it
 
 stock_tickers_with_template = {f"<FinGPTICKER_{ticker}>" for ticker in stock_tickers}
 
+
 # Export stock tickers
 import json
 import os
@@ -60,6 +67,9 @@ if __name__ == "__main__":
 
 
 """
+import yfinance as yf
+from tqdm import tqdm
+
 # Create dictionary {ticker: company_name}
 def get_company_name(ticker):
     try:
