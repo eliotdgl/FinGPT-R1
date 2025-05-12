86736fd95062639770a8287f1472b162754f96e7
eliotdgl <eliot.dugelay@gmail.com>
2025-05-12 15:49:27 +0200

    Continue training

--- FinGPTR1_tokenizer/training/FinGPTR1_tokenizer_training.py
+++ FinGPTR1_tokenizer/training/FinGPTR1_tokenizer_training.py
@@ -3,7 +3,7 @@ from torch import nn
 import os
 import json
 from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import json 
 
 from tokenization.preprocess_text import preprocess_text
@@ -20,8 +20,10 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
     print('\nImporting base tokenizer and model')
     # Load base tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained(base_model)
-    model = AutoModelForMaskedLM.from_pretrained(base_model).to(device)
+    model = AutoModelForSequenceClassification.from_pretrained(base_model).to(device)
 
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
     old_vocab_len = len(tokenizer)
 
@@ -39,7 +41,7 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
     with open("data/vocabulary/financial_vocab.json", "r") as f:
         financial_tokens = json.load(f)
 
-    """
+    
     def already_in_vocab(tokenizer, tokens):
         existing_vocab = set(tokenizer.get_vocab().keys())
         for token in tokens:
@@ -50,7 +52,7 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
     already_in_vocab(tokenizer, stock_tickers)
     already_in_vocab(tokenizer, num_tokens)
     already_in_vocab(tokenizer, financial_tokens)
-    """
+    
     
     # Add new tokens to the tokenizer
     tokenizer.add_tokens(stock_indices)
@@ -61,9 +63,6 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
 
     tokenizer.add_tokens(num_tokens)
 
-    if tokenizer.pad_token is None:
-        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    
     new_vocab_len = len(tokenizer)
 
 
@@ -83,7 +82,7 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
         list(Custom_Embeddings.new_embeddings_layer.parameters()) +
         list(Custom_Embeddings.num_mlp.parameters()) +
         list(Custom_Embeddings.unit_embed.parameters()),
-        lr=5e-5
+        lr=1e-5
     )
 
 
@@ -96,10 +95,14 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
     """
     train_data = data["train"].to_pandas()
     news = [headline for entry in train_data["news"].tolist() for headline in entry.split('\n')]
-    #labels = [label for entry in train_data["label"].tolist() for label in entry.split('\n')]
-    dataloader = DataLoader(news, batch_size=16, shuffle=True)
+    labels = [label for entry in train_data["label"].tolist() for label in entry.split('\n')]
+
+    label_map = {"Neutral": 0, "Rise": 1, "Fall": 2}  # Map sentiment classes
+    labels = [label_map[label] for label in labels]
+
+    dataloader = DataLoader(list(zip(news, labels)), batch_size=32, shuffle=True)
     
-    num_epochs = 1
+    num_epochs = 50
     epoch_bar = tqdm(range(num_epochs), position=0)
 
     # Training loop to train EmbeddingMLP() 
@@ -108,26 +111,21 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
 
         epoch_loss = 0
         batch_bar = tqdm(dataloader, position=1, leave=False)
-        for batch in batch_bar:
-            if isinstance(batch, str):
-                batch = [batch]
-            preprocessed_batch, batch_numbers_dict = zip(*[preprocess_text(text) for text in batch])
+        for batch_texts, batch_labels in batch_bar:
+            if isinstance(batch_texts, str):
+                batch_texts = [batch_texts]
+            
+            preprocessed_batch, batch_numbers_dict = zip(*[preprocess_text(text) for text in batch_texts])
 
             inputs = tokenizer(list(preprocessed_batch), padding=True, truncation=True, return_tensors="pt")
             batch_input_ids = inputs["input_ids"].to(device)
 
-            # Randomly mask 15% of tokens (except [PAD] tokens)
-            labels = batch_input_ids.clone()
-            rand = torch.rand(batch_input_ids.shape).to(device)
-            mask_arr = (rand < 0.15) * (batch_input_ids != tokenizer.pad_token_id)  # Mask 15% of tokens
-            labels[~mask_arr] = -100  # Set non-masked tokens to -100 (ignored in loss calculation)
-            batch_input_ids[mask_arr] = tokenizer.mask_token_id  # Replace with [MASK] token
-
             embeddings_batch, trainable = Custom_Embeddings(batch_input_ids, batch_numbers_dict)
-            
+
             if not trainable:
                 continue
-
+            
+            labels = torch.tensor(batch_labels).to(device)
             output = model(inputs_embeds=embeddings_batch, labels=labels)
             
             optimizer.zero_grad()
@@ -136,7 +134,7 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
             optimizer.step()
 
             epoch_loss += loss.item()
-        print(f"Training Tokenizer | Epoch {epoch} loss: {epoch_loss}")
+        print(f"Training Tokenizer | Epoch {epoch+1} loss: {epoch_loss}")
 
     print("Special tokenizer trained successfully")
 
@@ -151,7 +149,6 @@ def FGPTR1_training(base_model: str = "yiyanghkust/finbert-tone",
     tokenizer.save_pretrained('FinGPTR1_tokenizer/saved/tokenizer')
 
     torch.save(Custom_Embeddings.state_dict(), "FinGPTR1_tokenizer/saved/custom_embeddings/custom_embeddings.pt")
-    print("Tokenizer saved successfully")
 
     metadata = {
         "old_vocab_len": old_vocab_len,
