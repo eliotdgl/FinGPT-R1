f1e9a3baa72840923f5bf39d00f9a2c7f04ed672
Eliot <100833504+eliotdgl@users.noreply.github.com>
2025-06-26 01:14:17 +0200

    Update web_scraping_finance_news.py

--- data/financial_news/web_scraping_finance_news.py
+++ data/financial_news/web_scraping_finance_news.py
@@ -1,18 +1,15 @@
 """
+    == web_scraping_finance_news.py ==
     Quick script to get financial news from BusinessToday. 
-    It seems to be harder to get news for other well-known websites such as: Yahoo finance, CNBC, Bloomberg 
-    They seem to block automatic scrapping using simple requests. May have to use Selenium to simulate a website user
 """
-
-from bs4 import BeautifulSoup as BS
 import requests as req
+from bs4 import BeautifulSoup as BS
 
 def get_news(url: str, max_news: int) -> list[str]:
   webpage = req.get(url)
   soup = BS(webpage.content, "html.parser")
 
   news = []
-
   nb = 1
   for link in soup.find_all('a'):
     if nb > max_news:
@@ -27,6 +24,3 @@ bt_url = "https://www.businesstoday.in/latest/economy" # BusinessToday
 
 bt_news = get_news(bt_url, 50)
 print(bt_news)
-
-# Number of news we get is limited as we don't "Load More" news on the website. Again *Selenium* seems to be a more complete method
-len(bt_news)
